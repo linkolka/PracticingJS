73b3bdea5751188bbf690a537a524cff9b427dd0
Olga <48939564+linkolka@users.noreply.github.com>
2022-01-20 14:13:43 +0300

    Create practiseJS1.js

---
+++ practiseJS1.js
@@ -0,0 +1,12 @@
+const getNumber = 1;
+const GetAge = 2;
+const get_attemp = 0;
+
+alert(getNumber);
+alert(GetAge);
+alert(get_attemp);
+console.log(getNumber);
+console.log(GetAge);
+console.log(get_attemp);
+
+// alert выводит значение в модульном окне браузера в то время как console.log выводит сообщение в консоль.
