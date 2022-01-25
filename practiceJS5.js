343f6be97c0f4d26d97ca3bee19d0bd3e68e8220
Olga <48939564+linkolka@users.noreply.github.com>
2022-01-25 17:18:25 +0300

    Update and rename practiseJS5.js to practiceJS5.js

--- practiceJS5.js
+++ practiceJS5.js
@@ -2,6 +2,6 @@ let first = +prompt('enter first number', 0);
 let second = +prompt('enter second number', 0);
 
 let result = first > second ? first + second :
-							first == second ? 'числа одинаковые' :
-              first * second;
+	     first == second ? 'числа одинаковые' :
+             first * second;
 alert(result);
